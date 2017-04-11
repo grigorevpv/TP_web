b3a6539d5b4294d5458a60808f2140ed39f2fe6f
Pavel <grigorevpv@gmail.com>
2017-04-11 15:21:45 +0300

    add new views

--- ask/views.py
+++ ask/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 QUESTIONS = {
     '1': {'id': 1, 'title': 'I`m your dream', 'text': 'I`m your dream, make you real'},
@@ -23,6 +24,65 @@ def hot(request):
 	template = loader.get_template('ask/main_content.html')
 	questions = {}
 	tags = ['Moon', 'Park']
+	for i in xrange(1,55):
+		questions[i] = {
+			'title': 'title' + str(i),
+			'id': i,
+			'text': 'text' + str(i),
+			'tags': tags,
+			}
+	
+	questions = tuple(questions.items())
+	paginator = Paginator(questions, 4)
+	page = request.GET.get('page')
+	try:
+		questions = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		questions = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		questions = paginator.page(paginator.num_pages)
+
+	context = RequestContext(request, {
+		'questions': questions,
+		})
+	return HttpResponse(template.render(context))
+
+def tag(request, tagName):
+	template = loader.get_template('ask/tag_content.html')
+	questions = {}
+	tags = [tagName]
+	for i in xrange(1,43):
+		questions[i] = {
+			'title': 'title' + str(i),
+			'id': i,
+			'text': 'text' + str(i),
+			'tags': tags,
+			}
+
+	questions = tuple(questions.items())
+	paginator = Paginator(questions, 4)
+	page = request.GET.get('page')
+	try:
+		questions = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		questions = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		questions = paginator.page(paginator.num_pages)
+
+	context = RequestContext(request, {
+		'tagName':tagName,
+		'questions': questions,
+		})
+	return HttpResponse(template.render(context))
+
+def question(request, question):
+	template = loader.get_template('ask/question_content.html')
+	questions = {}
+	tags = ['Moon', 'Park']
 	for i in xrange(1,4):
 		questions[i] = {
 			'title': 'title' + str(i),
@@ -30,9 +90,21 @@ def hot(request):
 			'text': 'text' + str(i),
 			'tags': tags,
 			}
+	question = {'title': 'title', 'id': 1, 'text': 'text', 'tags': tags,}
 	context = RequestContext(request, {
+		'question':question,
 		'questions': questions.values(),
 		})
+	return HttpResponse(template.render(context))
+
+def login(request):
+	template = loader.get_template('ask/login_content.html')
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
+
+def signup(request):
+	template = loader.get_template('ask/signup_content.html')
+	context = RequestContext(request, {})
 	return HttpResponse(template.render(context))
 
 def list(request, list):
